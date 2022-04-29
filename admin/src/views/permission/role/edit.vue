14072d1990674490c2cb8fc1e3e71704d27c9719
Mrtangl <1807746833@qq.com>
2022-04-29 11:26:26 +0800

    角色排序缺少必填

--- admin/src/views/permission/role/edit.vue
+++ admin/src/views/permission/role/edit.vue
@@ -27,7 +27,7 @@
                     ></el-input>
                 </el-form-item>
                 <!-- 排序 -->
-                <el-form-item label="排序">
+                <el-form-item label="排序" prop="sort">
                     <el-input v-model="formData.sort" placeholder="请输入" type="number"></el-input>
                 </el-form-item>
                 <el-form-item label="状态">
@@ -115,6 +115,14 @@ export default defineComponent({
                     required: true,
                     message: '请输入角色名称',
                     trigger: ['blur']
+                }
+            ],
+            sort: [
+                {
+                    required: true,
+                    message: '请输入排序',
+                    trigger: 'blur',
+                    pattern: /(^[0-9]\d*$)/
                 }
             ]
         }
