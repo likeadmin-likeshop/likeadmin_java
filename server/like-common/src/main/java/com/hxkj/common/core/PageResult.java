280e6d34c5c02aeb73dfec0a8fec12a25ec5b6e6
TinyAnts <tinyants@163.com>
2022-06-09 18:32:44 +0800

    优化分页数据处理插件

--- server/like-common/src/main/java/com/hxkj/common/core/PageResult.java
+++ server/like-common/src/main/java/com/hxkj/common/core/PageResult.java
@@ -33,6 +33,24 @@ public class PageResult<T> {
     }
 
     /**
+     * PageHelper分页(数据额外处理)
+     *
+     * @author fzr
+     * @param list 分页数据集
+     * @param <T> 实体类型
+     * @return PageList
+     */
+    public static <T> PageResult<T> pageHelper(List<T> list, List<T> data) {
+        PageResult<T> pageResult = new PageResult<>();
+        PageInfo<T> pageInfo = new PageInfo<>(list);
+        pageResult.setCount(pageInfo.getTotal());
+        pageResult.setPageSize(pageInfo.getPageSize());
+        pageResult.setPageNo(pageInfo.getPageNum());
+        pageResult.setLists(data);
+        return pageResult;
+    }
+
+    /**
      * MyBatisPlus分页
      *
      * @author fzr
@@ -49,7 +67,7 @@ public class PageResult<T> {
     }
 
     /**
-     * MyBatisPlus分页
+     * MyBatisPlus分页(数据额外处理)
      *
      * @author fzr
      * @param <T> 实体类型
@@ -61,7 +79,6 @@ public class PageResult<T> {
         pageResult.setPageNo(Math.toIntExact(pageNo));
         pageResult.setPageSize(Math.toIntExact(size));
         pageResult.setLists(list);
-
         return pageResult;
     }
 
