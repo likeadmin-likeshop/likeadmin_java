65531299097c04c7284ac08a9131683c9618947a
TinyAnts <tinyants@163.com>
2022-08-10 16:10:06 +0800

    修复管理员一定要设置密码问题

--- server/like-admin/src/main/java/com/hxkj/admin/validate/system/SystemAuthAdminParam.java
+++ server/like-admin/src/main/java/com/hxkj/admin/validate/system/SystemAuthAdminParam.java
@@ -41,7 +41,7 @@ public class SystemAuthAdminParam implements Serializable {
     private String nickname;
 
     @NotEmpty(message = "密码不能为空", groups = {create.class})
-    @Length(min = 6, max = 32, message = "密码必须在6~32个字符内", groups = {create.class, update.class, upInfo.class})
+    @Length(min = 6, max = 32, message = "密码必须在6~32个字符内", groups = {create.class})
     private String password;
 
     @NotNull(message = "请选择状态", groups = {create.class, update.class})
