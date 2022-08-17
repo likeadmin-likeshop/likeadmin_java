6b8b046ccd200ad6366743ef1152112162d716e3
TinyAnts <tinyants@163.com>
2022-08-17 15:41:18 +0800

    解决刷新404问题

--- server/like-admin/src/main/java/com/hxkj/admin/LikeAdminApplication.java
+++ server/like-admin/src/main/java/com/hxkj/admin/LikeAdminApplication.java
@@ -4,13 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -25,14 +20,6 @@ public class LikeAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LikeAdminApplication.class, args);
-    }
-
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> myWebServerFactoryCustomizer(){
-        return factory -> {
-            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
-            factory.addErrorPages(error404Page);
-        };
     }
 
 }
---
+++ server/like-admin/src/main/java/com/hxkj/admin/config/ErrorPageConfig.java
@@ -0,0 +1,18 @@
+package com.hxkj.admin.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ErrorPageConfig implements ErrorPageRegistrar {
+
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage error404Page=new ErrorPage(HttpStatus.NOT_FOUND,"/index.html" );
+        registry.addErrorPages(error404Page);
+    }
+
+}
