dd4a57377ba484dcf1e9000c5140c192f6017c85
TinyAnts <tinyants@163.com>
2022-08-18 14:57:13 +0800

    调整免登录权限接口

--- server/like-admin/src/main/java/com/hxkj/admin/config/AdminConfig.java
+++ server/like-admin/src/main/java/com/hxkj/admin/config/AdminConfig.java
@@ -17,8 +17,7 @@ public class AdminConfig {
     // 免登录验证
     public static String[] notLoginUri = new String[]{
             "system:login",         // 登录接口
-            "common:index:config",  // 配置接口
-            "gen:downloadCode",  // 配置接口
+            "common:index:config"   // 配置接口
     };
 
     // 免权限验证
