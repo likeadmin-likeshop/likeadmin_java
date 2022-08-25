f5814b242a76a30685da787e87c20954c3571f10
TinyAnts <tinyants@163.com>
2022-08-25 18:36:46 +0800

    增加微信开放平台配置接口

--- server/like-admin/src/main/java/com/mdd/admin/controller/channel/WxController.java
+++ server/like-admin/src/main/java/com/mdd/admin/controller/channel/WxController.java
@@ -1,7 +1,13 @@
 package com.mdd.admin.controller.channel;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mdd.admin.config.aop.Log;
+import com.mdd.admin.service.channel.IChannelH5Service;
+import com.mdd.admin.service.channel.IChannelWxService;
+import com.mdd.common.core.AjaxResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * 微信开发平台渠道设置
@@ -9,4 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/channel/wx")
 public class WxController {
+
+    @Resource
+    IChannelWxService iChannelWxService;
+
+    /**
+     * H5渠道配置详情
+     *
+     * @author fzr
+     * @return Object
+     */
+    @GetMapping("/detail")
+    public Object detail() {
+        Map<String, Object> map = iChannelWxService.detail();
+        return AjaxResult.success(map);
+    }
+
+    /**
+     * H5渠道配置保存
+     *
+     * @author fzr
+     * @param param 参数
+     * @return Object
+     */
+    @Log(title = "微信开放平台渠道保存")
+    @PostMapping("/save")
+    public Object save(@RequestBody Map<String, String> param) {
+        iChannelWxService.save(param);
+        return AjaxResult.success();
+    }
+
 }
--- server/like-admin/src/main/java/com/mdd/admin/service/channel/IChannelWxService.java
+++ server/like-admin/src/main/java/com/mdd/admin/service/channel/IChannelWxService.java
@@ -1,4 +1,25 @@
 package com.mdd.admin.service.channel;
 
+import java.util.Map;
+
+/**
+ * 微信开放平台设置服务接口类
+ */
 public interface IChannelWxService {
+
+    /**
+     * 微信开放平台设置详情
+     *
+     * @author fzr
+     * @return Map<String, String>
+     */
+    Map<String, Object> detail();
+
+    /**
+     * 微信开放平台设置保存
+     *
+     * @author fzr
+     */
+    void save(Map<String, String> param);
+
 }
