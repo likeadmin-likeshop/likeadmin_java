db84c5bed513ecc28013b5ea821c1c69a7eb2909
洪树泽 <2393354236@qq.com>
2022-09-08 17:59:05 +0800

    【小程序】个人设置页面

---
+++ app/src/pages/user_set/user_set.vue
@@ -0,0 +1,30 @@
+<template>
+    <view class="user-set">
+        个人设置页面
+    </view>
+</template>
+
+<script setup lang="ts">
+import { getDecorate } from '@/api/shop'
+import { useUserStore } from '@/stores/user'
+import { onShow } from '@dcloudio/uni-app'
+import { storeToRefs } from 'pinia'
+import { reactive } from 'vue'
+const state = reactive<{
+    pages: any[]
+}>({
+    pages: []
+})
+const getData = async () => {
+    const data = await getDecorate({ id: 2 })
+    state.pages = JSON.parse(data.pages)
+}
+const userStore = useUserStore()
+const { userInfo, isLogin } = storeToRefs(userStore)
+onShow(() => {
+    userStore.getUser()
+})
+getData()
+</script>
+
+<style></style>
