11f85bd9554d6b9985c37ee76145386853072f53
洪树泽 <2393354236@qq.com>
2022-09-08 18:46:32 +0800

    【小程序】关于我们页面切图

--- app/src/pages/as_us/as_us.vue
+++ app/src/pages/as_us/as_us.vue
@@ -1,95 +1,20 @@
 <template>
-	<view class="user-set">
-		<view class="item flex bg-white mt-[20rpx]">
-			<u-avatar :src="src" shape="square"></u-avatar>
-			<view class="ml-[20rpx] flex flex-1 justify-between">
-				<view>
-					<view class="mb-[15rpx] text-xl font-medium">闻喜的小贾</view>
-					<view class="text-content text-xs">账号：tt54541512</view>
-				</view>
-				<u-icon name="arrow-right" color="#666"></u-icon>
-			</view>
-		</view>
-		<view class="item bg-white mt-[20rpx] btn-border flex flex-1 justify-between">
-			<view class="text-xl">登录密码</view>
-			<u-icon name="arrow-right" color="#666"></u-icon>
-		</view>
-		<view class="item bg-white btn-border flex flex-1 justify-between">
-			<view class="text-xl">绑定微信</view>
-			<view class=" flex justify-between">
-				<view class="text-muted mr-[20rpx]">
-					未绑定
-				</view>
-				<u-icon name="arrow-right" color="#666"></u-icon>
-			</view>
-		</view>
-		
-		<view class="item bg-white mt-[20rpx] btn-border flex flex-1 justify-between">
-			<view class="text-xl">隐私政策</view>
-			<u-icon name="arrow-right" color="#666"></u-icon>
-		</view>
-		<view class="item bg-white btn-border flex flex-1 justify-between">
-			<view class="text-xl">服务协议</view>
-			<u-icon name="arrow-right" color="#666"></u-icon>
-		</view>
-		<view class="item bg-white btn-border flex flex-1 justify-between">
-			<view class="text-xl">关于我们</view>
-			<view class="flex justify-between">
-				<view class="text-muted mr-[20rpx]">
-					v10.2.0
-				</view>
-				<u-icon name="arrow-right" color="#666"></u-icon>
-			</view>
-		</view>
+	<view class="as-us flex flex-1 flex-col items-center">
+		<image src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F02%2F2109242332225H9-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1665225789&t=d830459ba12f6416e919dd8082fc9d12" mode="" class="img"></image>
+		<view class="text-content mt-[20rpx]">当前版本v1.5.0</view>
 	</view>
 </template>
 
 <script setup lang="ts">
-	import {
-		getDecorate
-	} from '@/api/shop'
-	import {
-		useUserStore
-	} from '@/stores/user'
-	import {
-		onShow
-	} from '@dcloudio/uni-app'
-	import {
-		storeToRefs
-	} from 'pinia'
-	import {
-		reactive
-	} from 'vue'
-	const state = reactive < {
-		pages: any[]
-	} > ({
-		pages: []
-	})
-	const getData = async () => {
-		const data = await getDecorate({
-			id: 2
-		})
-		state.pages = JSON.parse(data.pages)
-	}
-	const userStore = useUserStore()
-	const {
-		userInfo,
-		isLogin
-	} = storeToRefs(userStore)
-	onShow(() => {
-		userStore.getUser()
-	})
-	getData()
 </script>
 
 <style lang="scss" scoped>
-	.user-set {
-		.item {
-			padding: 30rpx;
-		}
-
-		.btn-border {
-			border-bottom: 1rpx solid $u-form-item-border-color;
+	.as-us {
+		.img {
+			width: 160rpx;
+			height: 160rpx;
+			border-radius: 20rpx;
+			margin-top: 96rpx;
 		}
 	}
 </style>
