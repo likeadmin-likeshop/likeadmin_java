145f85edf7dba4cac0112d5a869dfe382c4aa5f3
洪树泽 <2393354236@qq.com>
2022-09-09 14:27:52 +0800

    【小程序】个人资料

--- app/src/utils/util.ts
+++ app/src/utils/util.ts
@@ -1,3 +1,5 @@
+ import { getToken } from './auth'
+
 /**
  * @description 获取元素节点信息（在组件中的元素必须要传ctx）
  * @param  { String } selector 选择器 '.app' | '#app'
@@ -21,5 +23,46 @@ export const getRect = (selector: string, all = false, context?: any) => {
                 reject('找不到元素')
             })
             .exec()
+    })
+}
+
+/**
+ * @description 上传图片
+ * @param  { String } path 选择的本地地址
+ */
+export function uploadFile(path: any) {
+	return new Promise((resolve, reject) => {
+		const token = getToken()
+		uni.uploadFile({
+			url: `${import.meta.env.VITE_APP_BASE_URL}/api/Upload/image`,
+			filePath: path,
+			name: 'file',
+			header: {
+				token,
+			},
+			fileType: 'image',
+			success: res => {
+				console.log('uploadFile res ==> ', res)
+				let data = JSON.parse(res.data);
+				if (data.code == 1) {
+					resolve(data.data);
+				} else {
+					reject()
+				}
+			},
+			fail: (err) => {
+				console.log(err)
+				reject()
+			}
+		});
+	});
+}
+
+export const getWxCode = (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        uni.login({
+            desc: '获取用户信息，完善用户资料',
+            success: (res: Event) => resolve(res.code)
+        })
     })
 }
