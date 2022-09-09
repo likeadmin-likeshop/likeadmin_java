d992e9bed056ff8a137ccdba6526dc95219f3224
洪树泽 <2393354236@qq.com>
2022-09-09 09:06:21 +0800

    协议页面对接

--- app/src/enums/agreementEnums.ts
+++ app/src/enums/agreementEnums.ts
@@ -1,6 +1,6 @@
 //菜单主题类型
 export enum AgreementEnum {
-    PRIVACY = '1',
-    SERVICE = '2'
+    PRIVACY = 'service',
+    SERVICE = 'privacy'
 }
 
--- app/src/pages/agreement/agreement.vue
+++ app/src/pages/agreement/agreement.vue
@@ -1,15 +1,38 @@
 <template>
 	<view class="">
-		<u-parse :html="content"></u-parse>
+		<u-parse :html="agreementContent"></u-parse>
 	</view>
 </template>
 
 <script setup lang="ts">
 	import { reactive, ref } from 'vue'
+	import { onLoad } from "@dcloudio/uni-app";
 	import { AgreementEnum } from '@/enums/agreementEnums'
-	const agreement = ref('') // 协议类型
+	import { getPolicy } from '@/api/app'
 	
-	
+	let agreementType = ref('') // 协议类型
+	let agreementContent = ref('') // 协议内容
+	// let agreementName = ref('') // 协议名称
+		
+	const getData = async (type) => {
+		let res = await getPolicy({ type })
+		console.log(res, 'res')
+		
+		agreementContent.value = res.content
+		
+		uni.setNavigationBarTitle({
+			title: res.name
+		})
+	}
+	    
+	onLoad((options: any) => {
+		// if(options.type) {
+		// 	agreementType.value = options.type
+		// 	getData(agreementType)
+		// }
+		
+		getData('service')
+	})
 </script>
 
 <style lang="scss" scoped>
