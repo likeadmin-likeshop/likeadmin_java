707180427d515d093d81818f516fca1b099381f5
mlzhang <1371582736@qq.com>
2022-09-10 22:35:06 +0800

    :fix: #I5QLSA

--- admin/src/views/organization/post/edit.vue
+++ admin/src/views/organization/post/edit.vue
@@ -22,9 +22,9 @@
                         <div class="form-tips">默认为0， 数值越大越排前</div>
                     </div>
                 </el-form-item>
-                <el-form-item label="备注" prop="remark">
+                <el-form-item label="备注" prop="remarks">
                     <el-input
-                        v-model="formData.remark"
+                        v-model="formData.remarks"
                         placeholder="请输入备注"
                         type="textarea"
                         :autosize="{ minRows: 4, maxRows: 6 }"
@@ -54,7 +54,7 @@ const formData = reactive({
     name: '',
     code: '',
     sort: 0,
-    remark: '',
+    remarks: '',
     isStop: 1
 })
 
