4945f57d15ee46a4bee9b5b7003b774bb168d096
mlzhang <1371582736@qq.com>
2022-09-10 22:41:18 +0800

    :bug: issues #I5QLRL

--- server/like-admin/src/main/java/com/mdd/admin/validate/system/SystemAuthPostParam.java
+++ server/like-admin/src/main/java/com/mdd/admin/validate/system/SystemAuthPostParam.java
@@ -7,6 +7,7 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -38,7 +39,8 @@ public class SystemAuthPostParam {
 
     @Length( max = 250, message = "岗位备注不能大于250个字符内", groups = {create.class, update.class})
     private String remarks = "";
-
+    @NotNull(message = "排序号不能为空", groups = {SystemAuthRoleParam.create.class, SystemAuthRoleParam.update.class})
+    @DecimalMin(value = "0", message = "排序号值不能少于0", groups = {SystemAuthAdminParam.create.class, SystemAuthAdminParam.update.class})
     private Integer sort = 0;
 
 }
