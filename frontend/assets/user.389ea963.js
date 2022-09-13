77b2b5ccb95c41bb9c9b233311eb8c485d70fa6a
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-13 10:23:33 +0800

    后台打包

--- frontend/assets/user.389ea963.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.ab5c8d8d.js";function s(){return t.get({url:"/setting/user/detail"})}function n(e){return t.post({url:"/setting/user/save",params:e})}function u(){return t.get({url:"/setting/login/detail"})}function i(e){return t.post({url:"/setting/login/save",params:e})}export{s as a,n as b,u as g,i as s};
