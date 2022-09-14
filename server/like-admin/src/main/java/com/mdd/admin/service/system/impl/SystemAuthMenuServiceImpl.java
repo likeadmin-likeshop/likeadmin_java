978580a00572dc2297dad3d24c5b306aa564b383
TinyAnts <tinyants@163.com>
2022-09-14 14:27:58 +0800

    修复菜单删除bug

--- server/like-admin/src/main/java/com/mdd/admin/service/system/impl/SystemAuthMenuServiceImpl.java
+++ server/like-admin/src/main/java/com/mdd/admin/service/system/impl/SystemAuthMenuServiceImpl.java
@@ -186,11 +186,17 @@ public class SystemAuthMenuServiceImpl implements ISystemAuthMenuService {
      */
     @Override
     public void del(Integer id) {
-        SystemAuthMenu model = systemAuthMenuMapper.selectOne(new QueryWrapper<SystemAuthMenu>().eq("id", id));
+        SystemAuthMenu model = systemAuthMenuMapper.selectOne(
+                new QueryWrapper<SystemAuthMenu>()
+                        .eq("id", id)
+                        .last("limit 1"));
+
         Assert.notNull(model, "菜单已不存在!");
 
         Assert.isNull(systemAuthMenuMapper.selectOne(
-                new QueryWrapper<SystemAuthMenu>().eq("pid", id)),
+                new QueryWrapper<SystemAuthMenu>()
+                        .eq("pid", id)
+                        .last("limit 1")),
                 "请先删除子菜单再操作！");
 
         systemAuthMenuMapper.deleteById(id);
