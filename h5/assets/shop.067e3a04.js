3f699d3c7c14b0c1ac997e0093742b484dccc7a5
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-16 18:23:03 +0800

    打包

---
+++ h5/assets/shop.067e3a04.js
@@ -0,0 +1,1 @@
+import{D as r}from"./index.24de5888.js";function t(){return r.get({url:"/index"})}function e(t){return r.get({url:"/decorate",data:t})}function n(){return r.get({url:"/hotSearch"})}function a(t){return r.get({url:"/search",data:t})}export{e as a,n as b,a as c,t as g};
