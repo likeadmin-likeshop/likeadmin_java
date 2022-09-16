2dfb13a69441f07e08ce866f373658dac5c41468
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-16 16:29:27 +0800

    后台打包

--- frontend/assets/system.45508e93.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.ccc78a7a.js";function s(){return t.get({url:"/monitor/server"})}function o(e){return t.get({url:"/system/log/operate",params:e})}function n(){return t.get({url:"/monitor/cache"})}export{s as a,o as b,n as s};
