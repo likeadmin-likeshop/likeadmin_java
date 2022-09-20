654c7b3c117666d66942ff809857dd720b2f009a
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-20 11:00:40 +0800

    后台打包

--- frontend/assets/user.ed84e35d.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.541a788f.js";function s(){return t.get({url:"/setting/user/detail"})}function n(e){return t.post({url:"/setting/user/save",params:e})}function u(){return t.get({url:"/setting/login/detail"})}function i(e){return t.post({url:"/setting/login/save",params:e})}export{s as a,n as b,u as g,i as s};
