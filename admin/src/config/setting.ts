51b35b664d170edf287fc52d7f732817ae8488bb
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-20 12:15:36 +0800

    调整备注

--- admin/src/config/setting.ts
+++ admin/src/config/setting.ts
@@ -1,13 +1,15 @@
 const defaultSetting = {
-    sideWidth: 200,
-    sideTheme: 'light',
-    sideDarkColor: '#1d2124',
-    theme: '#4A5DFF',
-    openMultipleTabs: true,
-    successTheme: '#67c23a',
-    warningTheme: '#e6a23c',
-    dangerTheme: '#f56c6c',
-    errorTheme: '#f56c6c',
-    infoTheme: '#909399'
+    sideWidth: 200, //侧边栏宽度
+    sideTheme: 'light', //侧边栏主题
+    sideDarkColor: '#1d2124', //侧边栏深色主题颜色
+    openMultipleTabs: true, // 是否开启多标签tab栏
+    theme: '#4A5DFF', //主题色
+    successTheme: '#67c23a', //成功主题色
+    warningTheme: '#e6a23c', //警告主题色
+    dangerTheme: '#f56c6c', //危险主题色
+    errorTheme: '#f56c6c', //错误主题色
+    infoTheme: '#909399' //信息主题色
 }
+//以上各种主题色分别对应element-plus的几种行为主题
+
 export default defaultSetting
