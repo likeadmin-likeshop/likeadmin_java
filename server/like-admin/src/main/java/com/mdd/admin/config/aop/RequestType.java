b20738061e479f78c6b67e5ebaaf2ea313570a96
mlzhang <1371582736@qq.com>
2022-09-25 03:39:58 +0800

    fixed 1d998d1 from https://gitee.com/zmldaniu/likeadmin_java/pulls/17 :bug: 增加日志类型、判断文件类型记录文件名称 #I5SYLB

---
+++ server/like-admin/src/main/java/com/mdd/admin/config/aop/RequestType.java
@@ -0,0 +1,17 @@
+package com.mdd.admin.config.aop;
+
+/**
+ * 请求参数类
+ */
+public enum RequestType {
+    /**
+     * 文件类型
+     */
+    File,
+    /**
+     * 默认数据类型
+     */
+    Default
+    ;
+
+}
