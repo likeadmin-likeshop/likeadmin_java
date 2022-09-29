a4b6b2d6c8f07ee672b15728004bae159ebfb0fa
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-29 10:26:49 +0800

    修改搜索栏input框宽度

--- admin/src/views/dev_tools/code/index.vue
+++ admin/src/views/dev_tools/code/index.vue
@@ -4,7 +4,7 @@
             <el-form class="mb-[-16px]" :model="formData" inline>
                 <el-form-item label="表名称">
                     <el-input
-                        class="w-56"
+                        class="w-[280px]"
                         v-model="formData.tableName"
                         clearable
                         @keyup.enter="resetPage"
@@ -12,7 +12,7 @@
                 </el-form-item>
                 <el-form-item label="表描述">
                     <el-input
-                        class="w-56"
+                        class="w-[280px]"
                         v-model="formData.tableComment"
                         clearable
                         @keyup.enter="resetPage"
