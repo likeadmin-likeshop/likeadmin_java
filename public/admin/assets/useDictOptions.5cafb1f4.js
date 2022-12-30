6f8e8442c0c0f721dec497d875b07e3cbec15ac4
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-12-30 11:41:58 +0800

    重新打包

--- public/admin/assets/useDictOptions.5cafb1f4.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/admin/assets/useDictOptions.b34b37f1.js
-import"./index.64e62b67.js";import{a0 as p,t as f}from"./@vue.cab01781.js";function d(a){const s=p({}),e=Object.keys(a),i=e.map(o=>{const t=a[o];return s[o]=[],()=>t.api(f(t.params)||{})}),r=async()=>{(await Promise.allSettled(i.map(t=>t()))).forEach((t,u)=>{const n=e[u];if(t.status=="fulfilled"){const{transformData:c}=a[n],l=c?c(t.value):t.value;s[n]=l}})};return r(),{optionsData:s,refresh:r}}export{d as u};
-========
-import"./index.a80222dc.js";import{a0 as p,t as f}from"./@vue.cab01781.js";function d(a){const s=p({}),e=Object.keys(a),i=e.map(o=>{const t=a[o];return s[o]=[],()=>t.api(f(t.params)||{})}),r=async()=>{(await Promise.allSettled(i.map(t=>t()))).forEach((t,u)=>{const n=e[u];if(t.status=="fulfilled"){const{transformData:c}=a[n],l=c?c(t.value):t.value;s[n]=l}})};return r(),{optionsData:s,refresh:r}}export{d as u};
->>>>>>>> wjx202212300933:public/admin/assets/useDictOptions.5cafb1f4.js
--- public/admin/assets/user.4d5749bd.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/admin/assets/user.a9344a7d.js
-import{r as t}from"./index.64e62b67.js";function s(){return t.get({url:"/setting/user/detail"})}function n(e){return t.post({url:"/setting/user/save",params:e})}function u(){return t.get({url:"/setting/login/detail"})}function i(e){return t.post({url:"/setting/login/save",params:e})}export{s as a,n as b,u as g,i as s};
-========
-import{r as t}from"./index.a80222dc.js";function s(){return t.get({url:"/setting/user/detail"})}function n(e){return t.post({url:"/setting/user/save",params:e})}function u(){return t.get({url:"/setting/login/detail"})}function i(e){return t.post({url:"/setting/login/save",params:e})}export{s as a,n as b,u as g,i as s};
->>>>>>>> wjx202212300933:public/admin/assets/user.4d5749bd.js
