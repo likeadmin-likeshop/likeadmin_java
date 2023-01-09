5d056280b0c91b065fa03b1c851eedbb727ebb95
Jason <5340635+wen-jason@user.noreply.gitee.com>
2023-01-09 19:02:20 +0800

    pc端

---
+++ pc/enums/appEnums.ts
@@ -0,0 +1,33 @@
+//菜单主题类型
+export enum ThemeEnum {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
+
+// 菜单类型
+export enum MenuEnum {
+    CATALOGUE = 'M',
+    MENU = 'C',
+    BUTTON = 'A'
+}
+
+// 屏幕
+export enum ScreenEnum {
+    SM = 640,
+    MD = 768,
+    LG = 1024,
+    XL = 1280,
+    '2XL' = 1536
+}
+
+export enum SMSEnum {
+    LOGIN = 101,
+    BIND_MOBILE = 102,
+    CHANGE_MOBILE = 103,
+    FIND_PASSWORD = 104
+}
+
+export enum PolicyAgreementEnum {
+    SERVICE = 'service',
+    PRIVACY = 'privacy'
+}
