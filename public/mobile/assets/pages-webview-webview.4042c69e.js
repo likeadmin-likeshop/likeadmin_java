41b4024fcfabd70652a7c7510472348f1c81e484
Jason <5340635+wen-jason@user.noreply.gitee.com>
2023-03-16 15:45:39 +0800

    调整收藏相关接口路径

--- public/mobile/assets/pages-webview-webview.4042c69e.js
+++
@@ -1,1 +0,0 @@
-import{d as a,D as e,W as s,o as n,c as o,aI as r}from"./index.5a0d49fa.js";const t=a({__name:"webview",setup(a){const t=e("");return s((a=>{t.value=decodeURIComponent(a.url)})),(a,e)=>{const s=r;return n(),o(s,{src:t.value},null,8,["src"])}}});export{t as default};
